637a850cf6aebcb3602353164dbd3809cc7e3343
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-11-07 10:00:38 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/BorderConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    internal class BorderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           var ckeckOk = (bool)value;
+           return ckeckOk ? 2 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
